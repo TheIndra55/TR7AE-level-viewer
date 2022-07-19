b0771c66b92a37bf91a509897c5c4b1704bf9125
Indra <wuppie55@gmail.com>
2022-07-19 02:23:16 +0200

    Don't relocate buffer twice

--- src/Section.ts
+++ src/Section.ts
@@ -50,7 +50,7 @@ class SectionList
 
 		const version = buffer.readInt32LE()
 
-		if (version != 14)
+		if (version != 14 && version != 15)
 		{
 			throw `Wrong DRM version, expected 14 but got ${version}`
 		}
@@ -95,6 +95,14 @@ class SectionList
             // skip past section data
 			this.buffer.skip(section.size);
 		}
+
+        // to prevent relocating a buffer again return if version has been set to 15
+        // this happens for example using Three.js FileLoader which returns the same buffer
+        if (version == 15)
+        {
+            return
+        }
+        buffer.buffer.writeUInt32LE(15, 0)
 
         // relocate file
         for (let section of this.sections)
